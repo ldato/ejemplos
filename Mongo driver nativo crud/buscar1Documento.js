68cae9cd9ed2aca3492d25aef6fc8efb4efcf689
Luciano Dato <lucianodato82@gmail.com>
2020-09-16 18:00:16 -0300

    Agregada otra forma de find con paramatro

--- Mongo driver nativo crud/buscar1Documento.js
+++ Mongo driver nativo crud/buscar1Documento.js
@@ -3,7 +3,7 @@ const { ObjectID } = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://127.0.0.1:27017";
 
-MongoClient.connect(url, (err, client) => {
+/* MongoClient.connect(url, (err, client) => {
   if (err) throw err;
   var db = client.db('base-prueba-crear-node');
   var consulta = db.collection("coleccion-crear-node").find({});//podemos agregar parametros como a un where
@@ -13,4 +13,14 @@ MongoClient.connect(url, (err, client) => {
     client.close();
   
 });
-})
+}) */
+// OTRA FORMA
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("base-prueba-crear-node");
+    dbo.collection("coleccion-crear-node").find({"clave3":12}).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
