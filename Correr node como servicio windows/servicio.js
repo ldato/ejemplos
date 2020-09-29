a4444f90ca57238f81737aa82a22f88e3c6c9d9b
Luciano Dato <lucianodato82@gmail.com>
2020-09-29 12:38:51 -0300

    Agrego correr node como servicio de windows

---
+++ Correr node como servicio windows/servicio.js
@@ -0,0 +1,17 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'Node application as Windows Service',
+  description: 'Node application as Windows Service',
+  script: 'index.js'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
+//svc.uninstall() para eliminar el servicio
